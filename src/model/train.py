5dfd6391928a87981d79dc8a22213e53de30bcd9
Tobias Mohr <48470377+machieser56@users.noreply.github.com>
2023-07-23 14:39:52 +0000

    Stage0: Corrected split_data function

--- src/model/train.py
+++ src/model/train.py
@@ -37,7 +37,8 @@ def get_csvs_df(path):
 
 # TO DO: add function to split data
 def split_data(df):
-    return train_test_split(df, test_size=0.30, random_state=0)
+    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+    return train_test_split(X, y, test_size=0.30, random_state=0)
 
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
